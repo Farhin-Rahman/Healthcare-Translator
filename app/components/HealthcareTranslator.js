0629e2100d5dc8c28c7a230b8bbeb76ebe7de8b2
Farhin Rahman <farhinrahman1998@gmail.com>
2025-04-17 02:25:18 +0600

    Fixed Issues

--- app/components/HealthcareTranslator.js
+++ app/components/HealthcareTranslator.js
@@ -175,7 +175,7 @@ export default function HealthcareTranslator() {
           }}
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
         >
-          
+          Test Speech
         </button>
       </div>
 
